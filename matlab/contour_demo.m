b2da92124a1f3a277a2c66ad5ece32a9ff8c79e9
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-06-08 15:51:52 +0200

    changes

--- matlab/contour_demo.m
+++ matlab/contour_demo.m
@@ -1,15 +1,16 @@
 function [] = contour_demo()
 img = imread(['/home/prittjam/src/gtrepeat/dggt/building_us.jpg']);
 [ny,nx,~ ] = size(img);
-keyboard;
 
 pts = DL.segment_contours(img);
 
 x = [pts(:).x];
+G = [pts(:).G]; 
+
 Gimg = nan(ny,nx);
 Gimg(sub2ind([ny nx],x(2,:),x(1,:))) = [pts(:).G]; 
 Gimg(isnan(Gimg)) = 0;
-cmap = distinguishable_colors(max(Gimg(:)));
+cmap = distinguishable_colors(max(G));
 rgb = label2rgb(Gimg,cmap,'k');
 
 figure;
