dfe709a06ef0b0dedecbff50a0c7b43d7b682e8e
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-06-09 14:26:50 +0200

    changes

--- matlab/contour_demo.m
+++ matlab/contour_demo.m
@@ -1,9 +1,9 @@
 function [] = contour_demo()
+dlines_init();
 img = imread(['/home/prittjam/src/gtrepeat/dggt/building_us.jpg']);
 [ny,nx,~ ] = size(img);
 
 pts = DL.segment_contours(img);
-Go = DL.group_orientations(pts);
 
 x = [pts(:).x];
 G = [pts(:).G]; 
@@ -13,21 +13,21 @@ Gimg(sub2ind([ny nx],x(2,:),x(1,:))) = [pts(:).G];
 Gimg(isnan(Gimg)) = 0;
 cmap = distinguishable_colors(max(G));
 rgb = label2rgb(Gimg,cmap,'k');
-
-figure;
-subplot(1,2,1);
-imshow(img);
-subplot(1,2,2);
 imshow(rgb);
 
-
-
-cmap = distinguishable_colors(max(Go));
-Gimg(sub2ind([ny nx],x(2,:),x(1,:))) = Go; 
-Gimg(isnan(Gimg)) = 0;
-rgb = label2rgb(Gimg,cmap,'k');
-figure;
-imshow(rgb);
-
-figure;
-quiver(x(1,iG),x(2,iG),u,v,0);
+%figure;
+%subplot(1,2,1);
+%imshow(img);
+%subplot(1,2,2);
+%imshow(rgb);
+%
+%Go = DL.group_orientations(pts);
+%cmap = distinguishable_colors(max(Go));
+%Gimg(sub2ind([ny nx],x(2,:),x(1,:))) = Go; 
+%Gimg(isnan(Gimg)) = 0;
+%rgb = label2rgb(Gimg,cmap,'k');
+%figure;
+%imshow(rgb);
+%
+%figure;
+%quiver(x(1,iG),x(2,iG),u,v,0);
--- matlab/dlines_init.m
+++ matlab/dlines_init.m
@@ -1,27 +1,25 @@
-function [] = dlines_init(src_path,opt_path)
+function [] = dlines_init(opt_path)
 if nargin < 1
-    src_path = '~/src/';
-end
-
-if nargin < 2
     opt_path = '~/opt/';
 end
 
 [cur_path, name, ext] = fileparts(mfilename('fullpath'));
 addpath([cur_path]);
 
+parent_path = fileparts(cur_path);
+
 if ~exist('calc_curvature','dir')
     addpath(fullfile([opt_path 'mex']));
 end
 
 if ~exist('edgesDemo','file')
-    addpath([src_path, '/edges']);
+    addpath([parent_path '/external/edges']);
 end
 
 if ~exist('cmp_splitapply','file')
-    addpath([src_path,'matlab_extras']);
+    addpath([parent_path '/external/matlab_extras']);
 end
 
 if ~exist('channels','dir')
-    addpath(genpath([src_path, '/dollar_toolbox']));
+    addpath(genpath([parent_path '/external/toolbox']));
 end
