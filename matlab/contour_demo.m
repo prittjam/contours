bd262ec3905713b0b53a8da5d8af29ff67ff2116
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-06-09 13:30:27 +0200

    changes

--- matlab/contour_demo.m
+++ matlab/contour_demo.m
@@ -3,6 +3,7 @@ img = imread(['/home/prittjam/src/gtrepeat/dggt/building_us.jpg']);
 [ny,nx,~ ] = size(img);
 
 pts = DL.segment_contours(img);
+Go = DL.group_orientations(pts);
 
 x = [pts(:).x];
 G = [pts(:).G]; 
@@ -18,3 +19,15 @@ subplot(1,2,1);
 imshow(img);
 subplot(1,2,2);
 imshow(rgb);
+
+
+
+cmap = distinguishable_colors(max(Go));
+Gimg(sub2ind([ny nx],x(2,:),x(1,:))) = Go; 
+Gimg(isnan(Gimg)) = 0;
+rgb = label2rgb(Gimg,cmap,'k');
+figure;
+imshow(rgb);
+
+figure;
+quiver(x(1,iG),x(2,iG),u,v,0);
