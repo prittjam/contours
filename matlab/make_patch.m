ec001a34333e25b94d82acc2181019c4809f58f4
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-04 15:20:22 +0200

    changes

--- matlab/make_patch.m
+++ matlab/make_patch.m
@@ -13,11 +13,6 @@ cfg.xsampling_freq = round(cfg.aspect_ratio*cfg.ysampling_freq);
 patch = [];
 Rp = [];
 
-border = [1 1; ...
-          size(img,2) 1; ...
-          size(img,2) size(img,1) ; ...
-          1 size(img,1);]';
-
 N = size([contour(:).x],2);
 ind = floor(linspace(1,N,cfg.xsampling_freq));
 
@@ -27,28 +22,23 @@ Y = zeros(cfg.ysampling_freq,cfg.xsampling_freq);
 [par_curves,n] = ...
     select_par_curves(contour,img,cfg.scale_list);
 
-for k = 1:size(X,2)
-    s = linspace(0, ...
-                 norm(par_curves.x1(:,ind(k))-par_curves.x2(:,ind(k))), ...
-                 cfg.ysampling_freq);
-    x2 = par_curves.x1(:,ind(k))+bsxfun(@times,s,n(:,ind(k)));
-    X(:,k) = x2(1,:);
-    Y(:,k) = x2(2,:);
-end
+if ~isempty(par_curves)
+    for k = 1:size(X,2)
+        s = linspace(0, ...
+                     norm(par_curves.x1(:,ind(k))-par_curves.x2(:,ind(k))), ...
+                     cfg.ysampling_freq);
+        x2 = par_curves.x1(:,ind(k))+bsxfun(@times,s,n(:,ind(k)));
+        X(:,k) = x2(1,:);
+        Y(:,k) = x2(2,:);
+    end
 
-if cfg.clip_border
-    in = inpolygon(X(:),Y(:),border(1,:),border(2,:)); 
-else
-    in = true;
-end
 
-if all(in)
     [xxn,yyn] = ...
         meshgrid(linspace(-cfg.aspect_ratio/2,cfg.aspect_ratio/2,cfg.xsampling_freq), ....
                  linspace(-0.5,0.5,cfg.ysampling_freq));
     fixed_points = [xxn(:) yyn(:)];
     moving_points = [X(:) Y(:)];
-
+    
     tform = fitgeotrans(moving_points,fixed_points,'lwm',12);
     
     R = imref2d([cfg.patch_ny cfg.patch_nx]);
@@ -57,21 +47,44 @@ if all(in)
     
     [patch,Rp] = imwarp(img,tform,'OutputView',R);
     patch = patch(end:-1:1,:,:);
-end 
-
+end
+    
 function [par_curves,n] = select_par_curves(contour,img,sc_list)
+border = [1 1; ...
+          size(img,2) 1; ...
+          size(img,2) size(img,1) ; ...
+          1 size(img,1);]';
+
+par_curves = [];
+n = [];
+
 if numel(sc_list) > 1
+    num_rgns = 0;
     grey_img = im2double(rgb2gray(img));
     for k = 1:numel(sc_list);
-        [par_curves_list(k),n] = ...
-            make_par_curves(contour,sc_list(k));    
-        rgn_stats(k) = ...
-            calc_entropy(par_curves_list(k),grey_img);
+        [par_curves,n] = make_par_curves(contour,sc_list(k));    
+        xx = [par_curves.x1 par_curves.x2];
+        in = inpolygon(xx(1,:),xx(2,:),border(1,:),border(2,:)); 
+        if all(in)
+            par_curves_list(k) = par_curves;
+            rgn_stats(k) = calc_entropy(par_curves_list(k), ...
+                                        grey_img);
+            num_rgns = num_rgns+1;
+        else
+            break;
+        end
     end
-    rgn_stats = calc_saliency(rgn_stats);
-    [ind,best_scale] = select_scale(rgn_stats);
-    par_curves = par_curves_list(ind);
+
+    if num_rgns == numel(sc_list)
+        rgn_stats = calc_saliency(rgn_stats);
+        [ind,best_scale] = select_scale(rgn_stats);
+        par_curves = par_curves_list(ind);
+    end
 else
-    [par_curves,n] = ...
-        make_par_curves(contour,sc_list);    
+    xx = [par_curves.x1 par_curves.x2];
+    in = inpolygon(xx(1,:),xx(2,:),border(1,:),border(2,:)); 
+    if all(in)
+        [par_curves,n] = ...
+            make_par_curves(contour,sc_list);    
+    end
 end
--- matlab/select_scale.m
+++ matlab/select_scale.m
@@ -1,10 +1,9 @@
 function [idx,best_scale] = select_scale(rgn_stats)
-fxx = [1 -2 1]/2;
-Hxx = conv([rgn_stats(:).H],fxx,'valid');
-ind = find(Hxx < 0)+1;
+%fxx = [1 -2 1]/2;
+%Hxx = conv([rgn_stats(:).H],fxx,'valid');
+Hxx = gradient(gradient([rgn_stats(:).H]));
+ind = find(Hxx < 0);
 Y = [rgn_stats(ind).Y];
 [~,ind2] = max(Y);
 idx = ind(ind2);
 best_scale = rgn_stats(idx).scale;
-
-keyboard;
