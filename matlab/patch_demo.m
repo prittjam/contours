08d4b1c0b9025f384974ec433e144ca21125c0f6
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-04 15:42:33 +0200

    changes

--- matlab/patch_demo.m
+++ matlab/patch_demo.m
@@ -1,38 +1,26 @@
-function [] = patch_demo(varargin)
-dlines_init();
-
+function [] = patch_demo(img,varargin)
 cfg = struct('scale_list', 30);
-cfg = cmp_argparse(cfg,varargin{:});
-
-[cur_path, name, ext] = fileparts(mfilename('fullpath'));
-parent_path = fileparts(cur_path);
-
-%img = imread('/home/old-ufo/dev/line_Jimmy/dlines/img/building_us.jpg');
-%img_fname = [parent_path '/img/pyramid.jpg']
-img_fname = ['img3.png'];
-img = imread(img_fname);
-[ny,nx,~ ] = size(img);
+cfg = cmp_argparse(cfg,varargin{:}); 
 
 E = DL.extract_contours(img);
-pts = DL.segment_contours(E);
+contour_list = DL.segment_contours(E);
 
-x = [pts(:).x];
-G = [pts(:).G]; 
+x = [contour_list(:).x];
+G = [contour_list(:).G]; 
 
 X = cmp_splitapply(@(x) { [x;ones(1,size(x,2))] }, ...
-                   [pts(:).x],[pts(:).G]);
+                   [contour_list(:).x],[contour_list(:).G]);
 Gsz  = cellfun(@(x) numel(x),X);
 [~,ind] = sort(Gsz,'descend');
-
-grey_img = im2double(rgb2gray(img));
 
 figure;
 imshow(img);
 
 num_patches = min(24,numel(Gsz));
 patch_list = zeros(41,73,3,num_patches);
+
 for k = 1:num_patches
-    contour = pts(G==ind(k));
+    contour = contour_list(G==ind(k));
     [patch,Rp,par_curves] = ...
         make_patch(contour,img, 'scale_list',cfg.scale_list);
     
