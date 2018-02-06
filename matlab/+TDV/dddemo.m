1a1e827b2170b45c0208d08b5eeb0eee490aada3
Dmytro Mishkin <ducha.aiki@gmail.com>
2018-02-06 11:07:35 +0100

    modified:   dddemo.m

--- matlab/+TDV/dddemo.m
+++ matlab/+TDV/dddemo.m
@@ -2,12 +2,14 @@ function [] = dddemo();
 [cur_pth, name, ext] = fileparts(mfilename('fullpath'));
 pth = [cur_pth '/../../data/'];
 datasets = dir(pth);
-datasets = datasets(3:end)
-for d=numel(datasets):-1:1
+%datasets = datasets(3:end)
+for d=numel(datasets)-2:-1:1
+%for d=1:numel(datasets)-2
 dataset = datasets(d).name
 %dataset = 'fountain';
 TDV.extract(pth,dataset);
 TDV.embed(pth,dataset,'T',7);
+y = 'matching'
 TDV.match(pth,dataset,'T',0.95);
 %TDV.show_matches(pth,dataset);
 end
